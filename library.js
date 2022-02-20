6beec65d9eaf8459eb7565d52ea0eed86d12190a
majormayday <minime222@gmail.com>
2022-02-19 21:58:59 -0700

    Update to classes.

--- library.js
+++ library.js
@@ -6,14 +6,15 @@ const readBookButtons = document.querySelectorAll('.readBut');
 
 let myLibrary = [];
 
-function Book(author,title,pages,read) {
+class Book {
+  constructor(author,title,pages,read){
+
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
-    return
   }
-
+}
 
   
   function addBookToLibrary() {
